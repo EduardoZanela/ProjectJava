e477273551ed74f4767d7a3dbfb01808b34cd209
EduardoZanela <eduardozanela@hotmail.com>
2016-06-11 01:48:33 -0300

    commit

--- src/main/java/com/br/project/controller/IndexController.java
+++ src/main/java/com/br/project/controller/IndexController.java
@@ -1,7 +1,15 @@
 package com.br.project.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,7 +21,41 @@ public class IndexController {
 	}
 	
 	@RequestMapping(value="/upload", method=RequestMethod.POST)
-	public @ResponseBody String handleFileUpload(@RequestParam("file") MultipartFile file){
+	public @ResponseBody ModelAndView handleFileUpload(@RequestParam("file") MultipartFile file){
+		if (!file.isEmpty()) {
+            try {
+
+                byte[] bytes = file.getBytes();
+
+                // Creating the directory to store file
+                String rootPath = System.getProperty("C://Users//Eduardo");
+                
+                System.out.println(file.getName());
+                
+                File dir = new File(rootPath + File.separator + "tmpFiles");
+
+                if (!dir.exists())
+                   dir.mkdirs();
+               
+               // Create the file on server
+               File serverFile = new File("C://Users//Eduardo" + File.separator + file.getOriginalFilename());
+               BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+               stream.write(bytes);
+               stream.close();
+
+
+               return index();
+
+            } 
+
+            catch (Exception e) {
+            	e.printStackTrace();
+                return new ModelAndView("error");
+            }
+        }      
+        else {
+            return index();
+        }
 	}
 	
 }
--- src/main/webapp/views/index.jsp
+++ src/main/webapp/views/index.jsp
@@ -9,13 +9,13 @@
 			<h1>Projeto Engenharia</h1>
 		</div>
 		<div class="formulario" style="display: block; text-align: center; width: 50%; margin: 15% auto 0 auto; ">
-			<form method="POST" action="/upload" enctype="multipart/form-data">
+			<form method="POST" action="/Project/upload" enctype="multipart/form-data">
 				File to upload: <input type="file" name="file"> 
 				<input type="submit" value="Upload">
 	</form>	
 		</div>
 		<div class="Relatorios" style="display: block; text-align: center; margin: 15% auto 0 auto; ">
-			<a href="/ralfabetica"><h2>Relatório de clientes em ordem alfabética</h2></a>
+			<a href="/Project/ralfabetica"><h2>Relatório de clientes em ordem alfabética</h2></a>
 			<a href="/rcidade"><h2>Relatório de clientes por ordem de cidade</h2></a>
 			<a href="/rpais"><h2>Relatório de clientes por ordem de País</h2></a>
 		</div>
